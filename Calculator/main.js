0cea98a550b120c7f41d9ac7d13b9014acd1fd7b
Rajpreet Singh Nayyar <rnayyar@tavisca.com>
2018-08-21 11:56:01 +0530

    first

---
+++ Calculator/main.js
@@ -0,0 +1,41 @@
+var flag=0;
+function ConcatNumber(val) {
+    if(flag==1)
+        {
+            ClearOrReplace("");
+            ClearOrReplace(val);
+            flag=0;
+        }
+    else
+        {
+document.getElementById("screen").value+=val;
+            flag=0;
+        }
+}
+function ClearOrReplace(val)
+{
+    document.getElementById("screen").value=val;
+}
+function Evaluate() 
+{ 
+
+    flag=1;
+    try 
+    { 
+        debugger;
+        ClearOrReplace(Evaluate(document.getElementById("screen").value));
+    } 
+    catch(e) 
+    {
+        ClearOrReplace('Error') 
+    } 
+}
+function Enter(event)
+    {
+     
+        if(event.keyCode=="13")
+        {
+            Evaluate();
+        }
+       
+    }
